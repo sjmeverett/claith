d387ea9ecb7ea76d18440fc6deda4618127015db
Stewart Everett <sjmeverett@gmail.com>
2018-06-27 21:46:04 +0100

    Fix button types

--- src/lib/buttons.tsx
+++ src/lib/buttons.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import { CommonProps, prefix, classes, BootstrapStyles } from './util';
-import { TriggerKeyProps, triggerKey } from './trigger-key';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { CommonProps, prefix, classes, BootstrapStyles } from "./util";
+import { TriggerKeyProps, triggerKey } from "./trigger-key";
+import { Link } from "react-router-dom";
 
 export interface ClickTargetProps extends CommonProps<any>, TriggerKeyProps {
   href?: string;
@@ -10,58 +10,53 @@ export interface ClickTargetProps extends CommonProps<any>, TriggerKeyProps {
   title?: string;
   disabled?: boolean;
   type?: string;
-};
+}
 
-export const clickTarget = 
-  (defaultElement: string | React.StatelessComponent<any>) =>
-  triggerKey()(
-    (props: ClickTargetProps) => {
-      const className = classes(props);
-      const {href, onClick, to, ...rest} = props;
-      let _props: ClickTargetProps = {...rest, className};
-      let element;
+export const clickTarget = (
+  defaultElement: string | React.StatelessComponent<any>
+) =>
+  triggerKey()((props: ClickTargetProps) => {
+    const className = classes(props);
+    const { href, onClick, to, ...rest } = props;
+    let _props: ClickTargetProps = { ...rest, className };
+    let element;
 
-      if (href) {
-        element = 'a';
-        _props.href = href;
-
-      } else if (onClick) {
-        element = 'button';
-        _props.onClick = onClick;
-
-      } else if (to) {
-        element = Link;
-        _props.to = to;
-
-      } else {
-        element = defaultElement;
-      }
-
-      return React.createElement(element, {..._props, role: 'button'});
+    if (href) {
+      element = "a";
+      _props.href = href;
+    } else if (onClick) {
+      element = "button";
+      _props.onClick = onClick;
+    } else if (to) {
+      element = Link;
+      _props.to = to;
+    } else {
+      element = defaultElement;
     }
-  );
 
-export const ClickTarget = clickTarget('div');
+    return React.createElement(element, { ..._props, role: "button" });
+  });
 
+export const ClickTarget = clickTarget("div");
 
 export interface ButtonProps extends ClickTargetProps, TriggerKeyProps {
   bsStyle?: BootstrapStyles;
   fab?: boolean;
   submit?: boolean;
-};
+}
 
 export const Button = (props: ButtonProps) => {
-  const {bsStyle, fab, submit, bg, fg, ...buttonProps} = props;
-  
+  const { bsStyle, fab, submit, bg, fg, ...buttonProps } = props;
+
   buttonProps.className = classes(
-    props, 'btn',
-    prefix(bsStyle, 'btn', 'secondary'), fab && 'fab'
+    props,
+    "btn",
+    prefix(bsStyle, "btn", "secondary"),
+    fab && "fab"
   );
 
-  if (submit) {
-    buttonProps.type = 'submit';
-  }
+  buttonProps.type = submit ? "submit" : "button";
 
-  const Target = clickTarget(submit ? 'button' : 'div');
-  return <Target {...buttonProps} />
+  const Target = clickTarget(submit ? "button" : "div");
+  return <Target {...buttonProps} />;
 };
--- src/lib/trigger-key.ts
+++ src/lib/trigger-key.ts
@@ -1,43 +1,41 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { CommonDivProps, prefix, classes } from './util';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { CommonDivProps, prefix, classes } from "./util";
 
 export interface TriggerKeyProps {
   triggerKey?: string;
-};
+}
 
+const decorator = (Component, keyHandler) =>
+  class KeyButton extends React.Component<any, any> {
+    componentWillMount() {
+      window.addEventListener("keydown", keyHandler, false);
+    }
 
-const decorator = (Component, keyHandler) => class KeyButton extends React.Component<any, any> {
-  componentWillMount() {
-    window.addEventListener('keydown', keyHandler, false);
-  }
+    componentWillUnmount() {
+      window.removeEventListener("keydown", keyHandler, false);
+    }
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', keyHandler, false);
-  }
+    render() {
+      const { keyHandler, ...props } = this.props;
+      return React.createElement(Component, props);
+    }
+  };
 
-  render() {
-    const {keyHandler, ...props} = this.props;
-    return React.createElement(Component, props);
-  }
-};
-
-
-export const triggerKey = 
-    (key?: string) => 
-    <T extends Object>(Component: React.ComponentClass<T> | React.StatelessComponent<T>): React.StatelessComponent<T & TriggerKeyProps> => {
-
+export const triggerKey = (key?: string) => <T extends Object>(
+  Component: React.ComponentClass<T> | React.StatelessComponent<T>
+): React.StatelessComponent<T & TriggerKeyProps> => {
   const _key = key;
 
-  return (props: (T & TriggerKeyProps)) => {
-    const {triggerKey, ...rest} = <TriggerKeyProps>props;
+  return (props: T & TriggerKeyProps) => {
+    const { triggerKey, ...rest } = <TriggerKeyProps>props;
     const key = triggerKey || _key;
     let element;
 
     if (key) {
-      const keyHandler = (event) => {
-        if (event.key === (triggerKey || _key)) {
-          element.dispatchEvent(new MouseEvent('click'));
+      const keyHandler = event => {
+        if (event.key === (triggerKey || _key) && element) {
+          element.dispatchEvent(new MouseEvent("click"));
           event.preventDefault();
         }
       };
@@ -46,13 +44,11 @@ export const triggerKey =
         ref(ref) {
           element = ref && ReactDOM.findDOMNode(ref);
         },
-        
+
         ...rest
       });
-
     } else {
-      return React.createElement(<any>Component, rest)
+      return React.createElement(<any>Component, rest);
     }
   };
 };
-
